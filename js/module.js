954a89e7ad2f793d2e4246c998b97b265ff15171
sxzhou <sxzhou@advent.com>
2016-10-09 13:50:21 +0800

    js patterns: module pattern

---
+++ js/module.js
@@ -0,0 +1,18 @@
+﻿var sequenceGen = (function () {
+    //
+    var prefix = 'ADVS';
+    var postFix = 'SSNC';
+
+    function generate() {
+        var randomStr = '';
+        for (var index = 0; index < 10; ++index) {
+            randomStr += Math.floor(Math.random() * 10);
+        }
+        return prefix + '-' + randomStr + '-' + postFix;
+    }   
+    return {
+        generate: generate
+    };
+})();
+
+console.log(sequenceGen.generate());
