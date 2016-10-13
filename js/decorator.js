6155f1250b8e9a47419b9bb40cfe76bcd8ef9cd5
godking <zhou.yang.god.king>
2016-10-13 22:12:10 +0800

    decorator pattern

---
+++ js/decorator.js
@@ -0,0 +1,41 @@
+﻿function Beverage() {//like abstract base class
+    this._cost = 0;
+}
+
+Beverage.prototype.cost = function () {
+    return this._cost;
+}
+
+//Coffee extends Beverage
+function Coffee() {
+    Beverage.call(this);
+    this._cost = 5;
+}
+
+Coffee.prototype = Object.create(Beverage.prototype);
+//from line 1 to 15, is inheritance pattern
+
+
+//BeverageDecorator extends Beverage
+function BeverageDecorator(beverage) {
+    Beverage.call(this);
+    this.beverage = beverage;
+}
+
+BeverageDecorator.prototype = Object.create(Beverage.prototype);
+BeverageDecorator.prototype.cost = function () {
+    return this._cost + this.beverage.cost();
+}
+
+
+//sugar extends BeverageDecorator
+function Sugar(beverage) {
+    BeverageDecorator.call(this, beverage);
+    this._cost = 0.15;
+}
+
+Sugar.prototype = Object.create(BeverageDecorator.prototype);
+
+var coffee = new Coffee();
+var coffee_sugar = new Sugar(coffee);
+
