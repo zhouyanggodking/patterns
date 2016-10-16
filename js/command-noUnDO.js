4b81750f82b0e678f8ea272d64b480721fcc8b47
godking <zhou.yang.god.king>
2016-10-16 15:40:14 +0800

    command pattern

---
+++ js/command-noUnDO.js
@@ -0,0 +1,33 @@
+﻿//fn: represented as reciever's action
+function Command(fn, value) {
+    this.execute = fn;
+    this.value = value;
+}
+
+//AddCommand extends Command
+function AddCommand(value) {
+    Command.call(this, function (val) {
+        return val + this.value;
+    }, value);
+}
+
+function SubCommand(value) {
+    Command.call(this, function (val) {
+        return val - this.value;
+    }, value);
+}
+
+function Calculator() {
+    this._currentValue = 0;
+}
+
+Calculator.prototype = {
+    execute: function (command) {
+        this._currentValue = command.execute(this._currentValue);
+    },
+    getCurrentValue: function () {
+        return this._currentValue;
+    }
+};
+
+var calc = new Calculator;
---
+++ js/command.js
@@ -0,0 +1,46 @@
+﻿//fn: represented as reciever's action
+function Command(fn, undo, value) {
+    this.execute = fn;
+    this.value = value;
+    this.undo = undo;
+}
+
+//AddCommand extends Command
+function AddCommand(value) {
+    Command.call(this, add, sub, value);
+}
+
+//SubCommand extends Command
+function SubCommand(value) {
+    Command.call(this, sub, add, value);
+}
+
+//named function to replace anonymous function
+function add(val) {
+    return val + this.value;
+}
+
+function sub(val) {
+    return val - this.value;
+}
+
+function Calculator() {
+    this._currentValue = 0;
+    this.commands = [];//executed command list
+}
+
+Calculator.prototype = {
+    execute: function (command) {
+        this._currentValue = command.execute(this._currentValue);
+        this.commands.push(command);
+    },
+    undo: function(){
+        var cmd = this.commands.pop();
+        this._currentValue = cmd.undo(this._currentValue);
+    },
+    getCurrentValue: function () {
+        return this._currentValue;
+    }
+};
+
+var calc = new Calculator;
